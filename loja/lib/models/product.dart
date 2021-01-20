9f58b9f411c586b6327618b427dc60ed60efe6cf
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-01-20 11:43:03 -0300

    models

---
+++ loja/lib/models/product.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  // esses valores finais precisam ser passados na hora da
+  // construção do produto
+  // e temos apenas um variavel dentro da classe que tem um
+  // valor padrão false
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+}
