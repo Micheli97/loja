cf9fbd3a1bdcde2f3b4fcfca5fb5212dcf842c75
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-01-25 22:05:36 -0300

    testando no android

--- loja/lib/views/product_form_screen.dart
+++ loja/lib/views/product_form_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class ProductFormScreen extends StatefulWidget {
@@ -8,9 +10,52 @@ class ProductFormScreen extends StatefulWidget {
 class _ProductFormScreenState extends State<ProductFormScreen> {
   @override
   Widget build(BuildContext context) {
+    final _priceFocusNode = FocusNode();
+    final _descriptionFocusNode = FocusNode();
+
+    @override
+    void disposed() {
+      super.dispose();
+      _priceFocusNode.dispose();
+      _descriptionFocusNode.dispose();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Título'),
+                textInputAction: TextInputAction.next, // para pular para o
+                // textform
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next, // para pular para o
+                // textform
+                focusNode: _priceFocusNode,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Descrição'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
