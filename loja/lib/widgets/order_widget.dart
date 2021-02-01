d485bc9a54827204795e0dae3ec863743777101f
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-02-01 11:44:33 -0300

    Correção - order_widget

--- loja/lib/widgets/order_widget.dart
+++ loja/lib/widgets/order_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:loja/providers/orders.dart';
-import 'package:loja/providers/product.dart';
 
 class OrderWidget extends StatefulWidget {
   final Order order;
@@ -22,7 +21,7 @@ class _OrderWidgetState extends State<OrderWidget> {
       child: Column(
         children: [
           ListTile(
-            title: Text('R\$ ${widget.order.total}'),
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
             subtitle: Text(
               DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
             ),
@@ -37,34 +36,35 @@ class _OrderWidgetState extends State<OrderWidget> {
           ),
           if (_expanded)
             Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 4,
-                ),
-                height: (widget.order.products.length * 25.0) + 10,
-                child: ListView(
-                  children: widget.order.products.map((product) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          product.title,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: (widget.order.products.length * 25.0) + 10,
+              child: ListView(
+                children: widget.order.products.map((product) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        product.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          '${product.quantity}x R\$ ${product.price}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey,
-                          ),
+                      ),
+                      Text(
+                        '${product.quantity}x R\$ ${product.price}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
                         ),
-                      ],
-                    );
-                  }).toList(),
-                ))
+                      ),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
         ],
       ),
     );
