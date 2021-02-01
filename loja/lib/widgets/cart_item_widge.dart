d6fff8d5da0364eab0982a6d893c995e97b180a8
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-02-01 11:41:54 -0300

    Correção - cart_item_widget

--- loja/lib/widgets/cart_item_widge.dart
+++ loja/lib/widgets/cart_item_widge.dart
@@ -4,7 +4,9 @@ import 'package:provider/provider.dart';
 
 class CartItemWidget extends StatelessWidget {
   final CartItem cartItem;
+
   CartItemWidget(this.cartItem);
+
   @override
   Widget build(BuildContext context) {
     return Dismissible(
