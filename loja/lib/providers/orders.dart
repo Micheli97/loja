e501ea0ac9e885084a58bb0a16af0c58d9cac985
Micheli97 <56922971+Micheli97@users.noreply.github.com>
2021-02-04 20:55:09 -0300

    Correção - orders

--- loja/lib/providers/orders.dart
+++ loja/lib/providers/orders.dart
@@ -1,7 +1,9 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:loja/providers/cart.dart';
+import 'package:http/http.dart' as http;
+import 'package:loja/utils/constants.dart';
 
 class Order {
   final String id;
@@ -18,6 +20,7 @@ class Order {
 }
 
 class Orders with ChangeNotifier {
+  final String _baseUrl = '${Constants.BASE_API_URL}/orders';
   List<Order> _items = [];
 
   List<Order> get items {
@@ -28,13 +31,62 @@ class Orders with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  Future<void> loadOrders() async {
+    List<Order> loadedItems = [];
+    final response = await http.get("$_baseUrl.json");
+    Map<String, dynamic> data = json.decode(response.body);
+
+    if (data != null) {
+      data.forEach((orderId, orderData) {
+        loadedItems.add(
+          Order(
+            id: orderId,
+            total: orderData['total'],
+            date: DateTime.parse(orderData['date']),
+            products: (orderData['products'] as List<dynamic>).map((item) {
+              return CartItem(
+                id: item['id'],
+                price: item['price'],
+                productId: item['productId'],
+                quantity: item['quantity'],
+                title: item['title'],
+              );
+            }).toList(),
+          ),
+        );
+      });
+      notifyListeners();
+    }
+
+    _items = loadedItems.reversed.toList();
+    return Future.value();
+  }
+
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final response = await http.post(
+      "$_baseUrl.json",
+      body: json.encode({
+        'total': cart.totalAmount,
+        'date': date.toIso8601String(),
+        'products': cart.items.values
+            .map((cartItem) => {
+                  'id': cartItem.id,
+                  'productId': cartItem.productId,
+                  'title': cartItem.title,
+                  'quantity': cartItem.quantity,
+                  'price': cartItem.price,
+                })
+            .toList()
+      }),
+    );
+
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: jsonDecode(response.body)['name'],
         total: cart.totalAmount,
-        date: DateTime.now(),
+        date: date,
         products: cart.items.values.toList(),
       ),
     );
---
+++ loja/lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const BASE_API_URL = '';
+}
